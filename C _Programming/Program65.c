e2e32a8287f23ee384a8752cabc0932078d22570
Akashgund13 <116616225+Akashgund13@users.noreply.github.com>
2023-05-18 20:47:31 +0530

    Add files via upload

---
+++ C _Programming/Program65.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool ChecKPallindrome(int iNo)
+{
+    int iDigit = 0;
+    int iReverse = 0;
+    int iTemp = iNo;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iReverse = (iReverse * 10) + iDigit;
+        iNo = iNo / 10;
+    }
+    if(iReverse == iTemp)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number : \n");
+    scanf("%d", &iValue);
+
+    bRet = ChecKPallindrome(iValue);
+    
+    if(bRet == true)
+    {
+        printf("%d is a pallindrome number\n", iValue);
+    }
+    else
+    {
+        printf("%d is not a pallindrome number\n", iValue);
+    }
+    return 0;
+}
