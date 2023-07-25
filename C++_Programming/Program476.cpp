0603411ffdb8a3e52a328ac32bd76e955f40a051
Akashgund13 <116616225+Akashgund13@users.noreply.github.com>
2023-07-25 11:35:58 +0530

    Add files via upload

---
+++ C++_Programming/Program476.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+
+        ArrayX(int);
+        ~ArrayX();
+        void Accept();
+        void Display();
+};
+
+ArrayX :: ArrayX(int iNo)
+{
+    cout<<"Inside constructor to allocate memory"<<"\n";
+    this->iSize = iNo;
+    this->Arr = new int[iSize];
+}
+
+ArrayX :: ~ArrayX()
+{
+    cout<<"Inside destructor to deallocate memory"<<"\n";
+    delete []Arr;
+}
+
+void ArrayX :: Accept()
+{
+    cout<<"Inside Accept method"<<"\n";
+
+    cout<<"Enter the elements :"<<"\n";
+
+    for(int iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        cin>>Arr[iCnt];
+    }
+}
+
+void ArrayX :: Display()
+{
+    cout<<"Inside Display method"<<"\n";
+
+    cout<<"Elements from tha array are :"<<"\n";
+
+    for(int iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        cout<<Arr[iCnt]<<"\t";
+    }
+    cout<<"\n";
+}
+
+
+int main()
+{
+    cout<<"Inside main"<<"\n";
+
+    int iValue = 0;
+
+    cout<<"Enter the size of an array :"<<"\n";
+    cin>>iValue;
+
+    ArrayX *obj = new ArrayX(iValue);       // Dynamically object creation
+
+    obj->Accept();
+    obj->Display();
+
+    delete obj;
+
+    cout<<"End of main"<<"\n";
+    
+    return 0;
+}
