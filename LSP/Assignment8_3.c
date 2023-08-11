622b0a10d295a9dceb21c117c3ec7ad4710c3c44
Akashgund13 <116616225+Akashgund13@users.noreply.github.com>
2023-08-11 20:11:56 +0530

    Add files via upload

---
+++ LSP/Assignment8_3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+    int ret1 = 0,  ret2 = 0, ret3 = 0;
+
+    ret1 = fork();
+    if(ret1 == 0)
+    {
+        execl("./Process1", "NULL", NULL);
+    }
+
+    ret2 = fork();
+    if(ret2 == 0)
+    {
+        excel("./Process2", "NULL", NULL);
+    }
+
+    ret3 = fork();
+    if(ret3 == 0)
+    {
+        excel("./Process3", "NULL", NULL);
+    }
+    
+    return 0;
+}
